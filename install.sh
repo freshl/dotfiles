8cc6d566bf5f5b3a7dfb0cbdbfd266e605f28fb9
sven <svengiebel@me.com>
2019-08-29 10:24:38 +0200

    fix install script for homebrew, fzf, and neovim

--- install.sh
+++ install.sh
@@ -4,16 +4,31 @@ DOTFILES=~/dotfiles
 echo "Installing some dotfiles :)"
 
 echo "->> Checking for Homebrew"
-# brew instsall neovim
-# brew install the_silver_searcher
+which -s brew
+if [[ $? != 0 ]] ; then
+		echo "->> Installing Homebrew"
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+		echo "->> Updating Homebrew"
+    brew update
+fi
+
+
+echo "->> Installing Packages"
+
+echo "->> Installing FZF"
+brew install fzf
+echo "->> Installung neovim"
+brew install neovim
 
 echo "->> Setting up Symlinks"
 
 echo "->>> VIM"
 ln -s $DOTFILES/vimrc ~/.vimrc
 
-echo "->>> TMUX"
-ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
+# echo "->>> TMUX"
+# ln -s $DOTFILES/tmux/tmux.conf ~/.tmux.conf
 
 echo "->> Symlinks FINISHED"
 echo "Installation done! Have fun coding :)"
