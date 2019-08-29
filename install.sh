e345979ca8afc6903b4eda11fe815733eabd7c26
sven <svengiebel@me.com>
2019-08-29 14:30:25 +0200

    add missing packages and brew installs, fix react-native to react-native-cli

--- install.sh
+++ install.sh
@@ -1,44 +1,97 @@
 #!/bin/bash
 DOTFILES=~/dotfiles
 
+echo "******************************"
 echo "Installing some dotfiles :)"
-
-# Checking node installation, exit if missing
-echo "->> Checking Node installation\n"
-which asd
-if [ $? -eq 0 ]; then
-  echo "--> Awesome! Node is installed. Lets continue\n"
-else
-  printf "\x1B[31mNode not found. Please install Node before executing this script :)\e[0m \n"
-    printf "\e[1mDownload NodeJS:\e[0"
-    printf "\e[4mhttps://nodejs.org/en/download/current/\e[0m"
-    exit 1;
-  fi
+echo
 
 ## check home brew installation, install if missing
-echo "->> Checking for Homebrew\n"
+echo "->> Checking for Homebrew"
+echo
 which -s brew
 if [[ $? != 0 ]] ; then
-		echo "->> Installing Homebrew\n"
+		echo "---> Installing Homebrew"
+    echo
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
-		echo "->> Updating Homebrew\n"
+		echo "---> Updating Homebrew"
+    echo
     brew update
 fi
 
-# Install brew packages
-echo "->> Installing Packages\n"
+# Checking node installation, exit if missing
+echo "->> Checking Node installation"
+echo
+which node
+if [ $? -eq 0 ]; then
+  echo "---> Awesome! Node is installed. Lets continue"
+  echo
+else
+  echo "---> Installing Node..."
+  brew install node
+fi
 
-echo "->> Installing FZF"
+# Install brew packages
+echo "******************************"
+echo "->> Checking Packages"
+echo
+
+which -s git
+if [[ $? != 0 ]] ; then
+		echo "--> Installing git"
+    echo
+    brew install git
+else
+    echo "--> Git found!"
+    echo
+fi
+
+echo "******************************"
+echo "->> Installing ReactNative dependencies"
+echo
+
+echo "---> Installing/Updating watchman"
+echo
+brew install watchman
+echo "---> Installing/Updating JDK"
+echo
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk8
+
+echo "---> Installing/Updating FZF"
+echo
 brew install fzf
-echo "->> Installing neovim"
+
+echo "---> Installing/Updating neovim"
+echo
 brew install neovim
 
-echo "->> Setting up Symlinks\n"
+
+# check global node packages
+echo "******************************"
+echo "->> Checking NPM Packages"
+echo
+
+echo "---> Installing/Updating Prettier..."
+echo
+npm install -g prettier
+
+echo "---> Installing/Updating ReactNative CLI..."
+echo
+npm install -g react-native-cli
+
+echo "---> Installing/Updating CRA..."
+echo
+npm install -g create-react-app
+
+
+echo "******************************"
+echo "->> Setting up Symlinks"
+echo 
 
 # Symlink stuff
-echo "->>> VIM"
+echo "---> vimrc"
 ln -s $DOTFILES/vimrc ~/.vimrc
 
 echo "Installation done! Have fun coding :)"
